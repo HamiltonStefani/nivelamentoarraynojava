344c271951e34d68da7dd32b3b5248fd73f8ad73
HamiltonStefani <hamiltonstefani1@gmail.com>
2023-05-04 20:12:56 -0300

    Nivelamnto Array no Java

---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("How many roons will be rented ? ");
+		int n = sc.nextInt();
+		
+		Rent[] vect = new Rent[10];
+		
+		for(int i = 1; i<=n; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Email: ");
+			String email = sc.next();
+			System.out.println("Room: ");
+			int roomNumber = sc.nextInt();
+			
+			vect[roomNumber] = new Rent(name, email);
+		}
+		
+		System.out.println();
+		System.out.println("Busy Roons: ");
+		
+		for (int i = 0; i<10; i++) {
+			if(vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
